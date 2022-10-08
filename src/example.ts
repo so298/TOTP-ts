02c62ffd49386ffdec39b644e927e960499695dd
SosukeHosokawa <so298go@gmail.com>
2022-10-08 18:18:06 +0900

    update example

--- src/example.ts
+++ src/example.ts
@@ -1,16 +1,6 @@
-# TOTP.ts
-
-A TypeScript implementation of RFC6238
-
-## examples
-
-Example is in `src/example.ts`
-
-```typescript
 import { generateTOTPFromUri } from "./";
 
 const uri = 
   "otpauth://totp/hello:world?secret=aaaaaaaa&issuer=so298";
 
 console.log(generateTOTPFromUri(uri));
-```
